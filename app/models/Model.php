b35061e2135af8cd161acb9ed0d648acaed4c99f
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-11-04 20:09:06 -0200

    PHP04112018 - Inclusões diversos.

---
+++ app/models/Model.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\models;
+
+abstract class Model{
+
+    protected $connection;
+   
+    public function __construct()
+    {
+        $this->connection = Connection::connect();
+    }
+
+    public function all() {
+        $sql = "SELECT * FROM {$this->table}";
+        $list = $this->connection->prepare($sql);
+        $list->execute();
+
+        return $list->fetchAll();
+    }
+
+    public function find($find, $value) {
+        $sql = "SELECT * FROM {$this->table} WHERE {$field} = ?";
+        $list = $this->connection->prepare($sql);
+        $list->bindValue(1, $value);
+        $list->execute();
+
+        return $list->fetch();
+    }
+
+    public function delete() {
+        $sql = "DELETE FROM {$this->table} WHERE $field = ?";
+        $delete = $this->connection->prepare($sql);
+        $delete->bindValue(1, $value);
+        $delete->execute();
+
+        return $delete->rowCount();
+    }
+}
