8c23f7cb52260493ee4465e7de5948148b17fcdc
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-11-04 20:00:39 -0200

    PHP04112018 - PDO parte I.

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -1,3 +1,8 @@
 <?php
 
+use app\models\User;
+
+$user = new User;
+dd($user->all());
+
 require "../app/views/index.php";
---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'host'      => '10.11.0.4',
+        'dbname'    => 'curso_pdo',
+        'username'  => 'root',
+        'password' => '123456A',
+        'charset'   => 'utf8'
+    ],
+];
