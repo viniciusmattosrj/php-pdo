55630dbc49362c28705396f4a5ead1c8efc6f896
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-11-04 19:59:47 -0200

    PHP04112018 - Criação inclusão SQL.

--- sql.sql
+++ sql.sql
@@ -1,3 +1,31 @@
 CREATE DATABASE curso_pdo;
 
-CREATE TABLE users(id int(11) auto
+CREATE TABLE users(
+    id int(11) auto_increment PRIMARY KEY,
+    name varchar(50) not null,
+    email VARCHAR(50) not null unique key not null,
+    password varchar(150) null,
+    created_at timestamp default current_timestamp
+);
+
+--Exibindo a estrutura da tabela
+desc users
+
+--Apagando tabela
+DROP TABLE users;
+
+CREATE TABLE posts(
+    id int(11) auto_increment PRIMARY KEY,
+    title varchar(50) not null,
+    user int(11) not null,
+    description text not null,
+    created_at timestamp default current_timestamp
+);
+
+--Exibindo a estrutura da tabela
+desc posts
+
+--Apagando tabela
+DROP TABLE posts;
+
+INSERT INTO users (name, email, password) VALUES('Vinicius','vinimattos.rj@gmail.com','123');
