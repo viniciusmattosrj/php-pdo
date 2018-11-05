9eeb4cceb0d4c8651524ededa2b725704191354d
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-11-04 22:12:23 -0200

    PHP04112018 - Adicionando comentários code.

--- app/classes/Routes.php
+++ app/classes/Routes.php
@@ -9,7 +9,7 @@ class Routes{
         if(!array_key_exists($uri, $routes)) {
             throw new \Exception("Rota não existe {$uri}");
         }
-
+        /* A extensão .php está sendo inserida aqui para evitar ficar tratando em cada arquivo. */
         return "../app/{$routes[$uri]}.php";
     }
 }
