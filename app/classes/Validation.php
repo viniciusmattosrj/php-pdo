582e69382ba017138495ea724fad65784f392b80
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-11-18 01:42:52 -0200

    PHP18112018 - Refatorando update e validando dados.

---
+++ app/classes/Validation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\classes;
+
+class Validation {
+
+    private $validate = [];
+
+    public function validate($post){
+
+        foreach($post as $key => $value) {
+            $this->validate[$key] = filter_var($value, FILTER_SANITIZE_STRING);
+        }
+
+        return (object) $this->validate;
+    }
+
+}
