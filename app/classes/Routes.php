20dd280b46e01ab61c343f884edd6205e0027a0c
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-11-20 14:56:37 -0200

    PHP20112018 - Implementando PSR-2.

--- app/classes/Routes.php
+++ app/classes/Routes.php
@@ -2,9 +2,10 @@
 
 namespace app\classes;
 
-class Routes {
-
-    public static function load($routes, $uri){
+class Routes
+{
+    public static function load($routes, $uri)
+    {
 
         if (!array_key_exists($uri, $routes)) {
             throw new \Exception("Rota não existe {$uri}");
